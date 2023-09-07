47b6e5b04b95f03caff85b79953b99722be6c39a
isaacasher97 <isaac.asher97@gmail.com>
2023-09-07 12:43:49 -0400

    Completed NFL queries 2

--- nfl.sql
+++ nfl.sql
@@ -3,26 +3,32 @@ SELECT name FROM teams;
 
 -- 2. List the stadium name and head coach of all NFC teams
 SELECT name, head_coach  FROM teams
-WHERE conference = 'NFC'
+WHERE conference = 'NFC';
 
 -- 3. List the head coaches of the AFC South
 SELECT head_coach  FROM teams
-WHERE conference = 'AFC' AND division ILIKE 'south'
+WHERE conference = 'AFC' AND division ILIKE 'south';
 
 -- 4. The total number of players in the NFL
-SELECT COUNT(*) FROM players
+SELECT COUNT(*) FROM players;
 
 -- 5. The team names and head coaches of the NFC North and AFC East
-
+SELECT name, head_coach FROM teams
+WHERE conference = 'AFC' AND division ILIKE 'east'
+OR conference = 'NFC' AND division ILIKE 'north';
 
 -- 6. The 50 players with the highest salaries
-
+SELECT name, salary FROM players
+ORDER BY salary DESC 
+LIMIT 50;
 
 -- 7. The average salary of all NFL players
+SELECT AVG(salary) FROM players -- Output ==> 	1579692.539817232376
 
 
 -- 8. The names and positions of players with a salary above 10_000_000
-
+SELECT name, position, salary FROM players
+WHERE salary > 10000000;
 
 -- 9. The player with the highest salary in the NFL
 
