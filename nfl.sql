970b3396a41ccde3ba6f177fa15851938579b0a4
isaacasher97 <isaac.asher97@gmail.com>
2023-09-07 13:17:52 -0400

    Completed HW NFL queries 3 done

--- nfl.sql
+++ nfl.sql
@@ -23,7 +23,7 @@ ORDER BY salary DESC
 LIMIT 50;
 
 -- 7. The average salary of all NFL players
-SELECT AVG(salary) FROM players -- Output ==> 	1579692.539817232376
+SELECT AVG(salary) FROM players -- Output ==> 1579692.539817232376
 
 
 -- 8. The names and positions of players with a salary above 10_000_000
@@ -31,18 +31,50 @@ SELECT name, position, salary FROM players
 WHERE salary > 10000000;
 
 -- 9. The player with the highest salary in the NFL
-
+SELECT * FROM players
+ORDER BY salary DESC
+LIMIT 1;
 
 -- 10. The name and position of the first 100 players with the lowest salaries
-
+SELECT name, position FROM players
+ORDER BY salary ASC
+LIMIT 100;
 
 -- 11. The average salary for a DE in the nfl
-
+SELECT AVG(salary) FROM players
+WHERE position ILIKE 'de'; -- Output ==> 2161326.377049180328
 
 -- 12. The names of all the players on the Buffalo Bills
+-- Answer 1 - without joining just using the team_id
+SELECT name
+FROM players
+WHERE team_id = 1;
+
+--Answer 2 - Doing an inner join and filtering list to SELECT just the Player name
+SELECT players.name FROM players
+JOIN teams
+ON players.team_id = teams.id
+WHERE team_id = 1
 
 
 -- 13. The total salary of all players on the New York Giants
-
+SELECT SUM(salary) FROM players
+WHERE team_id = 2;
 
 -- 14. The player with the lowest salary on the Green Bay Packers
+
+--Answer 1 - Not using Join
+SELECT * 
+FROM players
+WHERE team_id = 23
+ORDER BY salary ASC
+LIMIT 1
+
+--Answer 2 - Using Inner Join
+SELECT * 
+FROM players
+JOIN teams
+ON players.team_id = teams.id
+WHERE team_id = 23
+ORDER BY salary ASC
+LIMIT 1;
